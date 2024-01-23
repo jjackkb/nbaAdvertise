a3e7e8e1ffbcc81aa1907e8c8cfe4f1295b669d2
Camer <cameronschwing@icloud.com>
2024-01-23 09:54:39 -0800

    new class and file reader

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        int[] caps;
+
+        business b1 = new business("west", 1900, 3);
+        Object o = new JSONParser().parse(new FileReader(data.json));
+        JSONObject j = (JSONObject) o;
+
+        if (j.get(“coast”) >= b1.getSection()){
+            caps[] += j.get("capacities");
+        }
     }
 }
